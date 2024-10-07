629517faf99609bd60a3460a7de88536ccfe0992
Wolfpl2001 <Krystian.Korzeniewski@student.gildeopleidingen.nl>
2024-10-07 09:34:37 +0200

    Css and Readme.md

--- resources/views/tasks/Edit.blade.php
+++ resources/views/tasks/Edit.blade.php
@@ -3,18 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{'css/create.css'}}">
+    <link rel="stylesheet" href="{{ asset('css/create.css') }}">
     <title>Document</title>
 </head>
 <body>
-    <div class="bg">
-        <form action="{{route('tasks.edit', $id)}}" method="POST">
-            @csrf
-            @method('POST')
-            <input type="text" name="name" id="" placeholder="name" value="{{$name}}">
-            <input type="datetime-local" value="{{$date}}">
-            <button type="submit">Submit</button>
-        </form>
-    </div>
+<div class="bg">
+    <h1>Update Information</h1>
+    <form action="{{route('tasks.edit', $id)}}" method="POST">
+        @csrf
+        @method('POST')
+        <input type="text" name="name" id="" placeholder="name" value="{{$name}}">
+        <input type="datetime-local" value="{{$date}}">
+        <button type="submit">Submit</button>
+    </form>
+</div>
 </body>
 </html>
